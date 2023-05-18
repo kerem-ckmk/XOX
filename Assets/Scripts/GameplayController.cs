bb02985c711164a8b1f156d5815898ba2d3b6a9c
keremcakmakx <95014971+keremcakmakx@users.noreply.github.com>
2023-05-18 14:49:42 +0300

    a

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -7,17 +7,31 @@ public class GameplayController : MonoBehaviour
 {
     [Header("References")]
     public int maxGridSize = 8;
+    [Header("Sound")]
+    public AudioClip buttonClip;
+
     public int MatchScore { get; private set; }
     public bool IsInitialized { get; private set; }
+    public AudioSource AudioSource { get; private set; }
+
     public event Action<int> OnMatchScoreUpdate;
 
     public void Initialize()
     {
+        AudioSource = this.gameObject.AddComponent<AudioSource>();
+        AudioSource.playOnAwake = false;
+        AudioSource.clip = buttonClip;
+
         IsInitialized = true;
     }
 
     public void GridRebuild()
     {
 
+    }
+
+    public void PlaySound()
+    {
+        AudioSource.Play();
     }
 }
